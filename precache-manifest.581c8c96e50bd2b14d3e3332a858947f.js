ddb7c628d043655e4d88c764349e153418973509
R Narayan <nr.rnarayan@gmail.com>
2020-08-07 10:40:24 +0530

    Updates

---
+++ precache-manifest.581c8c96e50bd2b14d3e3332a858947f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db972bcc53208376afa8b6f8067ba895",
+    "url": "/Rick-and-Morty-react/index.html"
+  },
+  {
+    "revision": "8f3da23fa8dd6673cafa",
+    "url": "/Rick-and-Morty-react/static/css/main.b4d94eca.chunk.css"
+  },
+  {
+    "revision": "b1908ad2cef6a6e81e81",
+    "url": "/Rick-and-Morty-react/static/js/2.e016c46d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rick-and-Morty-react/static/js/2.e016c46d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f3da23fa8dd6673cafa",
+    "url": "/Rick-and-Morty-react/static/js/main.701c8288.chunk.js"
+  },
+  {
+    "revision": "d2fb8879cee761c87e89",
+    "url": "/Rick-and-Morty-react/static/js/runtime-main.68504e2c.js"
+  },
+  {
+    "revision": "5dc53c8e2c5bd8ccea56ebe7f0f3154a",
+    "url": "/Rick-and-Morty-react/static/media/bg.5dc53c8e.jpg"
+  },
+  {
+    "revision": "700ea584cc400f1846250967af0ad909",
+    "url": "/Rick-and-Morty-react/static/media/logo.700ea584.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/Rick-and-Morty-react/static/media/spinner.235b9b3e.gif"
+  }
+]);
